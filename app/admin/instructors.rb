858f44510add8530f2016ef9eb5c69a12b9864cd
Raghu Betina <raghu@firstdraft.com>
2019-03-19 16:33:05 +0000

    Whitelist columns in Instructor for ActiveAdmin

--- app/admin/instructors.rb
+++ app/admin/instructors.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Instructor do
+
+ permit_params :name, :image_url
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
